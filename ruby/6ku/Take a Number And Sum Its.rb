9b53e25c83d37d706eb2ad72164e6d44ef509bbb
Denis T <kiddypoker@gmail.com>
2023-12-28 13:08:38 +0300

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eurekagaa

---
+++ ruby/6ku/Take a Number And Sum Its.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+# https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/ruby
+
+def sum_dig_pow(a, b)
+  (a..b).select do |n|
+    n.digits.each_with_index.sum { |e, i| e**(i + 1) } == n
+  end
+end
+
+p sum_dig_pow(1, 100)
