7a1160fecb3fcaa305d05931335265f9e92306e5
Denis T <kiddypoker@gmail.com>
2023-12-29 00:19:37 +0300

    Distribute server workload

---
+++ ruby/7ku/Distribute server workload.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Distribute server workload
+# https://www.codewars.com/kata/59f22b3cf0a83ff3e3003d8c/train/ruby
+
+def distribute nodes, workload
+  groups = Array.new(nodes, workload / nodes)
+  (0...workload % nodes).each { |i| groups[i] += 1 }
+  jobs = (0...workload).to_a
+  groups.map { |n| jobs.shift n }
+end
+
+p distribute(2, 4)
---
+++ ruby/7ku/Doors in the school.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+# Doors in the school
+# https://www.codewars.com/kata/57c15d314677bb2bd4000017/train/ruby
+def doors(n)
+  Math.sqrt(n).to_i
+end
