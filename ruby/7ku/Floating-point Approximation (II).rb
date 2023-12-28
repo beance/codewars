fab074d612c9fc686696c1d409443e6775c65c99
Denis T <kiddypoker@gmail.com>
2023-12-28 19:49:27 +0300

    Floating-point Approximation (II)

---
+++ ruby/7ku/Floating-point Approximation (II).rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# Floating-point Approximation (II)
+# https://www.codewars.com/kata/581ee0db1bbdd04e010002fd/train/ruby
+
+def interp(f, l, u, n)
+  step = (u - l) / n.to_f
+  (0...n).map { |i| (100 * f.call(i * step + l)).floor / 100.0 }
+end
+
+p interp(lambda{|x| Math.sin(x)}, 0, 18.0, 12)
