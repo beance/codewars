96af9a406ba9a8002427ec9cd4f4b09be319e814
Denis T <kiddypoker@gmail.com>
2023-12-28 23:55:59 +0300

    English beggars

--- ruby/7ku/English beggars.rb
+++ ruby/7ku/English beggars.rb
@@ -1,2 +1,14 @@
 # frozen_string_literal: true
+# English beggars
+# https://www.codewars.com/kata/59590976838112bfea0000fa/train/ruby
+#
 
+def beggars(values, n)
+  return [] if n.zero?
+  arr = Array.new(n, 0)
+  values.each_with_index { |value, i| arr[i % n] += value }
+  arr
+end
+
+# p beggars([1,2,3,4,5],1) #[15]
+p beggars([1,2,3,4,5],1) #[9,6]
